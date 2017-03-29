412364ddb3f1e3f67c4cc5d78b3faa4f90f262cf
Max Spassky <maxspas1@gmail.com>
2017-03-29 18:47:07 +0300

    Product Cart 15%

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
 		$('.lang-switcher span').toggleClass('active')
 		// $(this).show()
 	})
+
+})
+
+$(window).on('load resize',function(){
+	var resizeProduct = function() {
+		var productContainer =  $('.img-inner-top').find('img').height();
+		var productBgMobile = '../' + $('.img-inner-top img').attr('src');
+		if ($(window).width() > 767) {
+			$('.category-box').css({
+				'height':productContainer,
+				'background-image':'none'
+			});
+		} else {
+			$('.category-box').css({
+				'height':'100%',
+				'background-image':'url('+productBgMobile+')'
+			})
+		}
+	}();
 })
