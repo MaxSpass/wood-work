6fe8fbb6a3370b21b800694182c2cd6895ffd34e
Max Spassky <maxspas1@gmail.com>
2017-03-31 10:20:00 +0300

    31.03.2017 commit

--- app/js/common.js
+++ app/js/common.js
@@ -8,17 +8,52 @@ $(document).ready(function(){
 
 $(window).on('load resize',function(){
 	var resizeProduct = function() {
-		var productContainer =  $('.img-inner-top').find('img').height();
-		var productBgMobile = '../' + $('.img-inner-top img').attr('src');
-		if ($(window).width() > 767) {
-			$('.category-box').css({
-				'height':productContainer,
+		var windowWidth = $(window).width();
+
+		var productContainerTop =  $('#img-inner-top').find('img').height();
+		var productBgMobile = '../' + $('#img-inner-top img').attr('src');
+		if (windowWidth > 767) {
+			$('#category-box').css({
+				'height':productContainerTop,
 				'background-image':'none'
 			});
 		} else {
-			$('.category-box').css({
+			$('#category-box').css({
 				'height':'100%',
 				'background-image':'url('+productBgMobile+')'
+			})
+		}
+
+		var productDesc = $('#product-desc');
+		var productProp = $('#product-prop').height();
+		var imgProductCenter1 = $('#img-product-center1');
+		var imgProductCenter1Src = '../' + $('#img-product-center1 img').attr('src');
+		var imgProductRight1 = $('#img-product-right1').height();
+		var deliveryInfo = $('#delivery-info');
+		
+		if (windowWidth > 767 && windowWidth < 1150) {
+			var imgProductCenter1Height = (imgProductRight1 + deliveryInfo) - productProp;
+			imgProductCenter1.css({
+				'height':imgProductCenter1Height,
+				'background-image': 'url('+imgProductCenter1Src+')'
+			})
+
+			var productDescHeight = imgProductRight1 + deliveryInfo.height() + 10;
+			productDesc.css({
+				'height': productDescHeight
+			})
+
+		} else {
+			imgProductCenter1.css({
+				'height':'100%',
+				'background-image':'none'
+			})
+			productDesc.css({
+				'height':'100%'
+			})
+			var deliveryInfoHeight = productDesc.height() - imgProductRight1;
+			deliveryInfo.css({
+				'height':deliveryInfoHeight
 			})
 		}
 	}();
